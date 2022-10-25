729064c24d45cc5b5278f6f40fd5d1be13984ecd
Nolan Leake <nolan@sigbus.net>
2022-10-25 09:40:35 -0700

    Add tests for resyncs after the daemon has timed out.

--- test.sh
+++ test.sh
@@ -34,9 +34,8 @@ git add "test dir/stagedfile"
 trap '$RP --killdaemon' EXIT
 $RP --init "$DST" || fail 98
 
-$RP true || fail 97 #Make sure the daemon has completed initial sync.
-
 #Test initial sync.
+$RP true || fail 97 #Make sure the daemon has completed initial sync.
 [ -f "$DST/.git/config" ] || fail 89
 [ -f "$DST/.git/info/exclude" ] || fail 88
 [ -f "$DST/file" ] || fail 87
@@ -54,20 +53,36 @@ mkdir -p "incr dir/a/b/c/d"
 touch "incr dir/a/b/c/file"
 touch ignored2
 rm file
+touch "incr dir/to delete 1"
+touch ".git/to delete 2"
 
 $RP true || fail 97
-
 [ -d "$DST/incr dir/a/b/c/d" ] || fail 79 #race in inotifywait for new directories.
 [ -f "$DST/incr dir/a/b/c/file" ] || fail 78
 [ ! -f "$DST/ignored" ] || fail 77
 [ ! -f "$DST/ignored2" ] || fail 76
 [ ! -f "$DST/file" ] || fail 75
+[ -f "$DST/incr dir/to delete 1" ] || fail 74
+[ -f "$DST/.git/to delete 2" ] || fail 73
+
+#Test non-incremental resyncs of an existing remote.
+$RP --killdaemon
+touch "test dir/non incremental"
+rm "incr dir/to delete 1"
+rm ".git/to delete 2"
+touch "ignored 3"
+
+$RP true || fail 97
+[ -f "$DST/test dir/non incremental" ] || fail 69
+[ ! -f "$DST/incr dir/to delete 1" ] || fail 68
+[ ! -f "$DST/.git/to delete 2" ] || fail 67
+[ ! -f "$DST/ignored 3" ] || fail 66
 
 #Check that repos are the same
-[ "$(find .git | wc -l)" -eq "$(find "$DST/.git" | wc -l)" ] || fail 69
+[ "$(find .git | wc -l)" -eq "$(find "$DST/.git" | wc -l)" ] || fail 19
 [ "$(git ls-files -co --exclude-standard | wc -l)" \
       -eq \
-      "$(git -C "$DST" ls-files -co --exclude-standard | wc -l)" ] || fail 68
+      "$(git -C "$DST" ls-files -co --exclude-standard | wc -l)" ] || fail 18
 
 #Cleanup
 if [ -n "$RP_TESTS_DUMP_LOGS" ]; then
